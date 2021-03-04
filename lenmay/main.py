80cdbd64c8a5c557ce8d7e9b34d91502dfeb1d72
KhanhIceTea <khanhicetea@gmail.com>
2021-03-04 17:29:52 +0700

    fix

--- lenmay/main.py
+++ lenmay/main.py
@@ -1,4 +1,5 @@
 import click
+import requests
 import os
 import sys
 import json
@@ -20,6 +21,21 @@ if os.path.isfile(settings_path):
     with open(settings_path, 'r') as f:
         init_settings = json.load(f)
 
+def current_ip_address():
+    r = requests.get('https://wtfismyip.com/text')
+    return r.text.strip() if r.status_code == 200 else None
+
+def resolve_dns(domain):
+    payload = {'name': domain, 'type': 'A'}
+    r = requests.get("https://dns.google/resolve", params=payload)
+    
+    if r.status_code == 200:
+        res = r.json()
+        if 'Answer' in res and 'data' in res['Answer'][0]:
+            return res['Answer'][0]['data']
+
+    return None
+
 def render_script(filepath, **context):
     template = jinja_env.get_template(filepath)
     return template.render(context)
@@ -30,6 +46,8 @@ def write_to_file(filepath, content, chmod=0o600):
     os.chmod(filepath, chmod)
 
 def run_script(script_content):
+    global dryRun
+
     tmp_file = "/tmp/lenmay-{}.sh".format(random.randint(10E9, 10E12))
     write_to_file(tmp_file, script_content)
     
@@ -56,6 +74,9 @@ def cli():
 @click.option('--default', is_flag=True, default=True, prompt='Default settings')
 @click.option('--dry', is_flag=True, prompt='Dry run?')
 def init(default, dry):
+    global dryRun
+    dryRun = dry
+
     if init_settings:
         return click.echo("Your system is already lenmay-ed, good luck!")
 
@@ -71,8 +92,6 @@ def init(default, dry):
         "jobber": True,
         "email": "",
     }
-
-    dryRun = dry
 
     if not default:
         settings["email"] = click.prompt("Email (for LetsEncrypt) ?")
@@ -106,22 +125,30 @@ def init(default, dry):
 @click.command()
 @click.option('--dry', is_flag=True, prompt='Dry run?')
 def web(dry):
+    global dryRun
+    dryRun = dry
+
     username = click.prompt("Username ? ")
     domains = click.prompt("Domain (multi domains separated by space)")
-    main_domain = domains.split()[0]
+    list_domains = domains.split()
+    main_domain = list_domains[0]
     le_email = click.prompt("Email for LetsEncrypt", init_settings["email"])
     root_dir = click.prompt("Document Root", "/home/{}/{}/public".format(username, main_domain))
-    mysql_db = click.prompt("DB Name (without prefix {}_ ,leave blank if no need)".format(username))
+    mysql_db = click.prompt("DB Name (without prefix {}_ ,leave blank if no need)".format(username), default='')
+
+    current_ip = current_ip_address()
+    for d in list_domains:
+        if resolve_dns(d) != current_ip:
+            return click.echo("Oops ! Please point the domain {} to {}".format(d, current_ip))
 
     mysql_random_password = random_string(12)
-    dryRun = dry
     ret = run_script_template("web/create_user.sh.twig", **init_settings, username=username, mysql_password=mysql_random_password)
     
     if ret == 0:
-        ret = run_script_template("web/create_site.sh.twig", **init_settings, username=username,
+        ret = run_script_template("web/create_site.sh.twig", **init_settings, username=username, list_domains=list_domains,
             domains=domains, main_domain=main_domain, le_email=le_email, root_dir=root_dir, mysql_db=mysql_db)
         if ret == 0:
-            click.echo("DONE !")
+            return click.echo("DONE !")
 
     click.echo("FAILED !")
 
